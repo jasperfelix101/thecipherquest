ffaa18cc367e2c1525c14995e8e53e982ce7a9c6
jasperfelix101 <166058279+jasperfelix101@users.noreply.github.com>
2024-10-23 16:30:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,28 +25,24 @@ app.listen(PORT, () => {
 const levels = [
     {
         image: "https://media.discordapp.net/attachments/1140857387864047637/1298330670883537106/Zodiac-e1608226062664.jpg?ex=67192c29&is=6717daa9&hm=b6e7406d0b4166ef393b3b3a8b5a76d4b44d3069d2e03637a3ce7a085ecd82f1&=&format=webp",
-        question: `Level 1: Three wheels spin: 6, 1, 3. Their positions align: 1, 17, 12. Reflect on B, and let A be constant. Can you decipher ttbqe ozyan vtmc `,
+        question: "Level 1: Three wheels spin: 6, 1, 3. Their positions align: 1, 17, 12. Reflect on B, and let A be constant. Can you decipher ttbqe ozyan vtmc",
         answer: "areyouenjoying"
     },
     {
         image: "https://media.discordapp.net/attachments/1140857387864047637/1298342915352956948/hq720_1.jpg?ex=67193791&is=6717e611&hm=285035505e6e0200bab925bdef02058600dabe00b38cbea47a26f7879b7b4d61&=&format=webp",
-        question: `Level 2: Hmm... 
-                   <br> <a href="https://forms.gle/jvGC6PEAmkGoGrw16">Fill your level 1 answer here!</a>`,
+        question: "Level 2: Hmm... ",
         answer: "diddy"
     },
     {
-        question: `Level 3: 1:1 6:7 4:2 11:5 4:1 8:8 1:1 14:2 3:1 14:7 1:3 .tiiny.site 
-                   <br> <a href="https://forms.gle/4V1VXCN6SrxCi6At9">Fill your level 2 answer here!</a>`,
+        question: "Level 3: 1:1 6:7 4:2 11:5 4:1 8:8 1:1 14:2 3:1 14:7 1:3 .tiiny.site",
         answer: "5259613"
     },
     {
-        question: `Level 4: Told you, anything and everything could be a clue. Guess what? It's right infront of you. 
-                   <br> <a href="https://forms.gle/Nia3XUYsYwfoQ9Qf9">Fill your level 3 answer here!</a>`,
+        question: "Level 4: Told you, anything and everything could be a clue. Guess what? It's right infront of you.",
         answer: "rossulbricht"
     },
     {
-        question: `Level 5: 
-                   <br> <a href="https://forms.gle/pNoCeV3BZmdtWEuK9">Fill your level 4 answer here!</a>`,
+        question: "Level 5: YUhSMGNITTZMeTkwYVc1NWRYSnNMbU52YlM4MWVYUjRNemhsZGc9PQ==",
         answer: "bribe"
     }
 ];
